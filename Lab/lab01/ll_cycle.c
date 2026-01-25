32805e3c253ef835dad1d46d703885391cf36059
tomato <2993150540@qq.com>
2026-01-25 23:36:21 +0800

    completed task: lab1

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,24 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL || head->next == NULL) {
+        return 0;
+    }
+
+    node *tortoise = head;
+    node *hare = head;
+
+    // loop contition: hare can move.
+    while (hare != NULL && hare->next != NULL) {
+        tortoise = tortoise->next;       // tortoise gose two steps.
+        hare = hare->next->next;         // hare goes two steps.
+
+        if (tortoise == hare) {
+            // if they meet, there will be a cycle.
+            return 1;
+        }
+    }
+
+    // no cycle
     return 0;
 }
