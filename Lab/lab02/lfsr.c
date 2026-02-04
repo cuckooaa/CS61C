5c7d49215305886091c32432108ea625e4f64d86
tomato <2993150540@qq.com>
2026-02-04 16:12:42 +0800

    completed task: lab02

--- Lab/lab02/lfsr.c
+++ Lab/lab02/lfsr.c
@@ -6,5 +6,14 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t bit0,bit2,bit3,bit5,temp;
+    bit0=*reg & 1;
+    bit2=*reg>>2 & 1;
+    bit3=*reg>>3 & 1;
+    bit5=*reg>>5 & 1;
+    temp=(bit5^(bit3^(bit2^bit0)));
+    *reg=*reg>>1;
+    if(temp==0) *reg &= ~(1<<15);
+    else *reg |= 1<<15;
 }
 
